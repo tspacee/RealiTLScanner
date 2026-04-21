d84b2a094de90a7285b519cc209b2c97079e887f
Tyler Space <tspacee@users.noreply.github.com>
2026-04-21 11:16:39 +0800

    feat(geo): also try GeoLite2-Country.mmdb as fallback db path

--- geo.go
+++ geo.go
@@ -16,13 +16,17 @@ func NewGeo() *Geo {
 	geo := &Geo{
 		mu: sync.Mutex{},
 	}
-	reader, err := geoip2.Open("Country.mmdb")
-	if err != nil {
-		slog.Warn("Cannot open Country.mmdb, GeoIP lookup will be disabled")
+	// Try GeoLite2-Country.mmdb as a fallback in addition to Country.mmdb
+	for _, dbPath := range []string{"Country.mmdb", "GeoLite2-Country.mmdb"} {
+		reader, err := geoip2.Open(dbPath)
+		if err != nil {
+			continue
+		}
+		slog.Info("Enabled GeoIP", "db", dbPath)
+		geo.geoReader = reader
 		return geo
 	}
-	slog.Info("Enabled GeoIP")
-	geo.geoReader = reader
+	slog.Warn("Cannot open Country.mmdb or GeoLite2-Country.mmdb, GeoIP lookup will be disabled")
 	return geo
 }
 
